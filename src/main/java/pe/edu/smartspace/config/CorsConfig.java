cd21de53129313fadf04fdd86dac02c7245afc8f
rober <robertojs_03@hotmail.com>
2025-12-01 03:29:45 -0500

    Usuario service y apppropert para tamaño 3d

--- src/main/java/pe/edu/smartspace/config/CorsConfig.java
+++ src/main/java/pe/edu/smartspace/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","https://smartspaceai.netlify.app")   // tu Angular
+                        .allowedOrigins("https://smartspaceai.netlify.app")   // tu Angular "http://localhost:4200",
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
